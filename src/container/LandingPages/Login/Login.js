9776aebf0598ccba51cfb9616208009587283266
ashaytiwari <ashaytiwari2001@gmail.com>
2021-11-15 12:08:46 +0530

    config: Add Material ui Library, created prettier json file, font families setup

---
+++ src/container/LandingPages/Login/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Login.module.scss";
+
+const Login = () => {
+  return (
+    <div>
+      <h4 className={styles.title}>Hello Login</h4>
+    </div>
+  );
+};
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./assets/FontFamilies/Lato-Black_0.ttf";
+import "./assets/FontFamilies/Lato-Bold_0.ttf";
+import "./assets/FontFamilies/Lato-Light_0.ttf";
+import "./assets/FontFamilies/Lato-Regular_0.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
