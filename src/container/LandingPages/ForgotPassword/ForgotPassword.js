4ecea7ab7d0ef4e02842c7f4e4452601067222b0
ashaytiwari <ashaytiwari2001@gmail.com>
2021-12-22 12:41:15 +0530

    feat: Forgot Password Implemented

--- src/container/LandingPages/ForgotPassword/ForgotPassword.js
+++ src/container/LandingPages/ForgotPassword/ForgotPassword.js
@@ -1,26 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ForgotPassword.module.scss";
 import Logo from "../../../assets/images/logo.png";
 import InputField from "../../../component/UI/InputField/InputField";
 import Button from "../../../component/UI/Button/Button";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { useFormik } from "formik";
+import { emailValidation } from "../../../utils/formValidations/formValidations";
+import { forgotPassword } from "../../../services/authServices/authServices";
+import { useSnackbar } from "notistack";
+import Loader from "../../../component/Loader/Loader";
 
 const ForgotPassword = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.email) {
+      errors.email = t("formValidationMessage:required");
+    } else if (emailValidation(values.email)) {
+      errors.email = t("formValidationMessage:emailValid");
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: ""
+    },
+    validate,
+    onSubmit: () => {
+      setIsLoading(true);
+      handleForgotPassword();
+    }
+  });
+
+  const handleForgotPassword = () => {
+    const param = {
+      email_id: formik.values.email
+    };
+    forgotPassword(param).then((res) => {
+      if (res.data.statuscode === 200) {
+        localStorage.setItem("EmailAuth", formik.values.email);
+        enqueueSnackbar(res.data.message, {
+          variant: "success"
+        });
+        formik.resetForm();
+        setIsLoading(false);
+      } else {
+        enqueueSnackbar(res.data.message, {
+          variant: "error"
+        });
+        setIsLoading(false);
+      }
+    });
+  };
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <div className={styles.wrapper}>
       <img src={Logo} alt="logo" />
       <div className={styles.body}>
         <h3>{t("landingPages:forgotYourPassword")}</h3>
-        <p>
+        <p className={styles.description}>
           {t("landingPages:pleaseEnter")}
           <br />
           {t("landingPages:willEmail")}
         </p>
-        <form>
+        <form onSubmit={formik.handleSubmit}>
           <div className={styles.formGroup}>
             <InputField
               id={"email"}
@@ -28,6 +84,11 @@ const ForgotPassword = () => {
               label={t("form:email")}
               variant={"outlined"}
               type={"email"}
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.errors.email && formik.touched.email ? true : false}
+              helperText={formik.errors.email}
+              onBlur={formik.handleBlur}
             />
           </div>
           <div className={styles.formGroup}>
--- src/services/authServices/authServices.js
+++ src/services/authServices/authServices.js
@@ -1,13 +1,19 @@
 import axios from "../../axios/axios";
 
 export const loginService = async (data) => {
-  const url = "https://develop.hipoz.com/api/commanloginuser";
+  const url = "https://beelytical.kritin.tech/api/commanloginuser";
   const res = await axios.post(url, data);
   return res;
 };
 
 export const registrationService = async (data) => {
-  const url = "https://develop.hipoz.com/api/signup";
+  const url = "https://beelytical.kritin.tech/api/signup";
+  const res = await axios.post(url, data);
+  return res;
+};
+
+export const forgotPassword = async (data) => {
+  const url = "https://beelytical.kritin.tech/api/commanforgotpassword";
   const res = await axios.post(url, data);
   return res;
 };
