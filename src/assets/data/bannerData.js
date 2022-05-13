d6e48b4a25cc0f55b6fb7aa45cfb0e996a407909
ashaytiwari <ashaytiwari2001@gmail.com>
2022-05-13 17:44:45 +0530

    chore: data json added for banners

---
+++ src/assets/data/bannerData.js
@@ -0,0 +1,39 @@
+import Banner1 from '../images/banner/banner1.jpeg';
+import Banner2 from '../images/banner/banner2.jpg';
+import Banner3 from '../images/banner/banner3.jpeg';
+import Banner4 from '../images/banner/banner4.jpeg';
+import Banner5 from '../images/banner/banner5.jpg';
+import Banner6 from '../images/banner/banner6.jpg';
+
+export const bannerData = [
+  {
+    imageURL: Banner1,
+    title: 'Machine Learning is fun!',
+    url: 'https://www.machinelearningisfun.com/'
+  },
+  {
+    imageURL: Banner2,
+    title: 'Last days at sea and arriving in Noumea, New Caledonia',
+    url: 'https://www.oceanblogs.org/geotraces/2022/04/11/last-days-at-sea-and-arriving-in-noumea-new-caledonia/'
+  },
+  {
+    imageURL: Banner3,
+    title: 'Aditi Learning Centre: A model for “Private Protected Area',
+    url: 'https://www.forrest-india.org/aditi-learning-centre-a-model-for-private-protected-area/'
+  },
+  {
+    imageURL: Banner4,
+    title: 'Apple Machine Learning Journal',
+    url: 'https://machinelearning.apple.com/'
+  },
+  {
+    imageURL: Banner5,
+    title: 'Dwarf Galaxies in the local group and beyond',
+    url: 'https://spaceandbeyondbox.com/dwarf-galaxies-in-the-local-group-and-beyond/'
+  },
+  {
+    imageURL: Banner6,
+    title: 'Top 10 Amazing Ocean Mysteries and Phenomena',
+    url: 'https://www.marineinsight.com/environment/top-10-amazing-ocean-mysteries-and-phenomena/'
+  }
+];
